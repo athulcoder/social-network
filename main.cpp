ee4a5204232953e24744f32aaadaa694c779c976
athulcoder <athul.sin.cos.tan@gmail.com>
2025-09-24 20:35:07 +0530

    add friend

--- main.cpp
+++ main.cpp
@@ -27,13 +27,66 @@ public:
     {
         cout << "User ID : " << id << " Username : " << name << endl;
     }
-
-}
+};
 
 class Graph
 {
-    int v;
-    unordered_map<User, vector<User>> adj;
 
 public:
+    int v;
+    vector<User> usersList;
+    unordered_map<int, vector<int>> adj;
+
+    Graph()
+    {
+        v = 0;
+    }
+
+    void addUser(string username)
+    {
+
+        usersList.push_back(User(v, username));
+    }
+
+    void addFriend(int u, int v, bool direction = false)
+    {
+        // default we set the friends are undirectional
+        adj[u].push_back(v);
+
+        if (!direction)
+            adj[v].push_back(u);
+    }
+
+    void displayFriends(int userId)
+    {
+
+        User user = usersList[userId];
+        cout << "Friends of " << user.name << " are : " << endl;
+
+        for (int firendId : adj[userId])
+        {
+            User frnd = usersList[firendId];
+            cout << "\t >" << frnd.name << endl;
+        }
+    }
+
+    // This function is used to show mutual friends between to users
+    void findMutualFriends(int userId_1, int userId_2)
+    {
+    }
+};
+
+int main()
+{
+    Graph g;
+    g.addUser("alice");
+    g.addUser("bob");
+    g.addUser("athul");
+    g.addUser("arun");
+
+    g.addFriend(0, 1);
+    g.addFriend(0, 2);
+    g.addFriend(0, 3);
+
+    g.displayFriends();
 }
